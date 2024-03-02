f4d36801d050a6f3c60fa3ae4be3d08fa699133a
singhalharsh10 <singhalharsh23@gmail.com>
2024-03-02 15:38:05 +0530

    INITIAL COMMIT

--- doctor-appointment/src/main/java/com/harsh/doctorappointment/driver/Driver/Driver.java
+++ doctor-appointment/src/main/java/com/harsh/doctorappointment/driver/Driver/Driver.java
@@ -6,9 +6,11 @@ import com.harsh.doctorappointment.entity.TimeSlot;
 import com.harsh.doctorappointment.service.AppointmentService;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Driver {
+
 
 //    public static TimeSlot convertToTimeSlot(String timeSlot) throws Exception {
 //        String[] times = timeSlot.split("-");
@@ -26,64 +28,145 @@ public class Driver {
 
 
     public static void main(String[] args) throws Exception {
+
         AppointmentService appointmentService = new AppointmentService();
 
         // Register Doctors
-        appointmentService.registerDoctor("Harsh", Speciality.CARDIOLOGIST);
-        appointmentService.registerDoctor("Soumen", Speciality.DERMATOLOGIST);
-
-        List<TimeSlot> harshAvailability = Arrays.asList(
-                new TimeSlot(9.5, 10.0),
-                new TimeSlot(12.5, 13.0),
-                new TimeSlot(16.0, 16.5)
-        );
-
-
-        appointmentService.markDoctorAvailability("Harsh", harshAvailability);
-
-        List<TimeSlot> soumenAvailability = Arrays.asList(
-                new TimeSlot(9.5, 10.0),
-                new TimeSlot(12.5, 13.0),
-                new TimeSlot(16.0, 16.5)
-        );
-        appointmentService.markDoctorAvailability("Soumen", soumenAvailability);
-
-
-        // Show Available Slots by Speciality
-        System.out.println("Available Cardiologist Slots:");
-        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
-                .forEach(slot -> System.out.println("Dr.Harsh: (" + slot.getStartTime() + "-" + slot.getEndTime() + ")"));
-
-        TimeSlot slot = new TimeSlot(12.5,13.0);
-        // Book Appointment
-        int bookingResult = appointmentService.bookAppointment(1234,"PatientA", "Harsh", slot);
-        System.out.println(bookingResult);
-
+        appointmentService.registerDoctor("Curious", Speciality.CARDIOLOGIST);
 //
-//        int result = appointmentService.bookAppointment(2345,"PatientA", "Soumen", slot);
-//        System.out.println(result);
+        List<TimeSlot> harshAvailability = List.of(
+                new TimeSlot(12.5, 13.5)
+        );
+        appointmentService.markDoctorAvailability("Curious", harshAvailability);
+//
+//
+//        harshAvailability = Arrays.asList(
+//                new TimeSlot(9.5, 10.0),
+//                new TimeSlot(12.5, 13.0),
+//                new TimeSlot(16.0, 16.5)
+//        );
+//        appointmentService.markDoctorAvailability("Curious", harshAvailability);
+//
+//        System.out.println();
+//
+//        // Show Available Slots by Speciality
+//        System.out.println("Available Cardiologist Slots:");
+//        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
+//                .forEach(doctor -> {
+//                    List<TimeSlot> timeSlots = doctor.getAvailability();
+//                    timeSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
+//                    timeSlots.forEach(slot1 -> System.out.println("Dr. " + doctor.getName() + ":( " + slot1.getStartTime() + "-" + slot1.getEndTime() + " )"));
+//
+//                });
+//
+//        System.out.println();
+//
+//
+//        appointmentService.registerDoctor("Dreadful", Speciality.DERMATOLOGIST);
+//        List<TimeSlot> soumenAvailability = Arrays.asList(
+//                new TimeSlot(9.5, 10.0),
+//                new TimeSlot(12.5, 13.0),
+//                new TimeSlot(16.0, 16.5)
+//        );
+//        appointmentService.markDoctorAvailability("Dreadful", soumenAvailability);
+//
+//
+//        // Show Available Slots by Speciality
+//        System.out.println("Available DERMATOLOGIST Slots:");
+//        appointmentService.getAvailableSlotsBySpeciality(Speciality.DERMATOLOGIST)
+//                .forEach(doctor -> {
+//                    List<TimeSlot> timeSlots = doctor.getAvailability();
+//                    timeSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
+//                    timeSlots.forEach(slot1 -> System.out.println("Dr." + doctor.getName() + ":( " + slot1.getStartTime() + "-" + slot1.getEndTime() + " )"));
+//
+//                });
+//
+//
+//        TimeSlot slot = new TimeSlot(12.5, 13.0);
+//
+//        System.out.println();
+//        // Book Appointment for Patient A for above time slot
+//        int bookingResult = appointmentService.bookAppointment(1234, "PatientA", "Curious", slot);
+//        System.out.println("Booked, Booking id -> " + bookingResult);
+//
+//        System.out.println();
+//        System.out.println("Available Cardiologist Slots:");
+//        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
+//                .forEach(doctor -> {
+//                    List<TimeSlot> timeSlots = doctor.getAvailability();
+//                    timeSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
+//                    timeSlots.forEach(slot1 -> System.out.println(doctor.getName() + ":( " + slot1.getStartTime() + "-" + slot1.getEndTime() + " )"));
+//
+//                });
+//
+//        System.out.println();
+//
+//        System.out.println("Patient A Appointments");
+//
+//        List<Appointment> appointments = appointmentService.seeAppointments("PatientA", false);
+//
+//        appointments.forEach(appointment -> {
+//            System.out.print(appointment.getDoctor().getName() + ":");
+//            System.out.print("( " + appointment.getTimeSlot().getStartTime());
+//            System.out.print("-->");
+//            System.out.println(appointment.getTimeSlot().getEndTime() + " )");
+//        });
+//
+//        System.out.println();
+//        System.out.println("Doctor Curious Appointments");
+//        appointments = appointmentService.seeAppointments("Curious", true);
+//        appointments.forEach(appointment -> {
+//            System.out.print(appointment.getPatient().getName() + ":");
+//            System.out.print("( " + appointment.getTimeSlot().getStartTime());
+//            System.out.print("-->");
+//            System.out.println(appointment.getTimeSlot().getEndTime() + " )");
+//        });
+//
+//
+//        // Booking the slot for same patient for same slot
+////        int result = appointmentService.bookAppointment(2345,"PatientA", "Soumen", slot);
+////        System.out.println(result);
+//
+//
+////        Cancel Appointment
+//        appointmentService.cancelBooking(1234);
+//
+//        System.out.println();
+//        System.out.println("Available Cardiologist Slots: ");
+//        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
+//                .forEach(doctor -> {
+//                    List<TimeSlot> timeSlots = doctor.getAvailability();
+//                    timeSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
+//                    timeSlots.forEach(slot1 -> System.out.println(doctor.getName() + ":( " + slot1.getStartTime() + "-" + slot1.getEndTime() + " )"));
+//
+//                });
+//
+//
+//        bookingResult = appointmentService.bookAppointment(5678, "PatientB", "Curious", slot);
+//        System.out.println("Booked, Booking id -> " + bookingResult);
+//
+//
+//        System.out.println();
+//        System.out.println("Available Cardiologist Slots: ");
+//        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
+//                .forEach(doctor -> {
+//                    List<TimeSlot> timeSlots = doctor.getAvailability();
+//                    timeSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
+//                    timeSlots.forEach(slot1 -> System.out.println(doctor.getName() + ":( " + slot1.getStartTime() + "-" + slot1.getEndTime() + " )"));
+//
+//                });
+//
+//
+//        appointmentService.registerDoctor("Daring", Speciality.DERMATOLOGIST);
+//
+//        List<TimeSlot> daringAvailability = List.of(
+//                new TimeSlot(17.5, 18.0)
+//        );
+//        appointmentService.markDoctorAvailability("Daring", daringAvailability);
+//
+//
+//    }
 
-
-
-        System.out.println("Available Cardiologist Slots:");
-        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
-                .forEach(slot1 -> System.out.println("Dr.Harsh: (" + slot1.getStartTime() + "-" + slot1.getEndTime() + ")"));
-
-//        Cancel Appointment
-//        appointmentService.cancelBooking(1234 );
-
-
-        System.out.println("Available Cardiologist Slots:");
-        appointmentService.getAvailableSlotsBySpeciality(Speciality.CARDIOLOGIST)
-                .forEach(slot1 -> System.out.println("Dr.Harsh: (" + slot1.getStartTime() + "-" + slot1.getEndTime() + ")"));
-
-
-
-        List<Appointment> appointments = appointmentService.seeAppointments("Harsh", true);
-        appointments.forEach(appointment -> {
-            System.out.println(appointment.getTimeSlot().getStartTime());
-            System.out.println(appointment.getTimeSlot().getEndTime());
-        });
 
     }
 }
--- doctor-appointment/src/main/java/com/harsh/doctorappointment/service/AppointmentService.java
+++ doctor-appointment/src/main/java/com/harsh/doctorappointment/service/AppointmentService.java
@@ -1,5 +1,6 @@
 package com.harsh.doctorappointment.service;
 
+import ch.qos.logback.core.joran.sanity.Pair;
 import com.harsh.doctorappointment.entity.*;
 
 import java.util.*;
@@ -9,7 +10,8 @@ public class AppointmentService {
     private List<Doctor> doctors;
     private List<Patient> patients;
     private Map<Integer, Appointment> appointments;
-    private Map<String, Queue<Patient>> waitlist;
+
+    private Map<String, Queue<Appointment>>waitlist;
 
     public AppointmentService() {
         this.doctors = new ArrayList<>();
@@ -19,26 +21,45 @@ public class AppointmentService {
     }
 
 
-
     public void registerDoctor(String name, Speciality speciality) throws Exception {
         Doctor doctor = new Doctor(name, speciality );
         Optional<Doctor> isDoctorAlreadyPresent = doctors.stream().filter(existingDoctor-> Objects.equals(existingDoctor.getName(), name)).findFirst();
         if(isDoctorAlreadyPresent.isPresent()){
-            throw  new Exception("Doctor is ALready Registered with same name");
+            throw  new Exception("Doctor is Already Registered with same name");
         }
         doctors.add(doctor);
-        System.out.println("Doctor Registered with name"+ name +" and speciality "+ speciality);
+        System.out.println("Welcome Dr. "+ name);
+        System.out.println();
 
     }
 
     public void markDoctorAvailability(String doctorName,List<TimeSlot>slots) {
+        System.out.println("Trying to mark availability of Doctor "+doctorName);
         Doctor doctor = findDoctorByName(doctorName);
+
         slots.forEach(slot->{
-            assert doctor != null;
-            doctor.addAvailability(slot);
+
+            if((slot.getEndTime()-slot.getStartTime())==0.5) {
+                assert doctor != null;
+                doctor.addAvailability(slot);
+
+            }else{
+                System.out.println("Sorry Dr. "+ doctorName+" solts are 30 minutes only");
+                System.out.println();
+            }
         });
 
+        System.out.println("Done Doc.");
+
+
+
     }
+
+    private void addToWaitlist(Appointment appointment, String doctorName) {
+        Queue<Appointment> waitQueue = waitlist.computeIfAbsent(doctorName, k -> new LinkedList<>());
+        waitQueue.add(appointment);
+    }
+
 
 //    public List<TimeSlot> showAvailabilityBySpeciality(String speciality) {
 //        List<TimeSlot> availableSlots = new ArrayList<>();
@@ -51,23 +72,37 @@ public class AppointmentService {
 //        return availableSlots;
 //    }
 
-    public List<TimeSlot> getAvailableSlotsBySpeciality(Speciality speciality) {
-        List<TimeSlot> availableSlots = new ArrayList<>();
+    public List<Doctor> getAvailableSlotsBySpeciality(Speciality speciality) {
+        List<Doctor> availabledoctors = new ArrayList<>();
         for (Doctor doctor : doctors) {
             if (doctor.getSpeciality() == speciality) {
-                availableSlots.addAll(doctor.getAvailability());
+                availabledoctors.add(doctor);
             }
         }
-        availableSlots.sort(Comparator.comparingDouble(TimeSlot::getStartTime));
-        return availableSlots;
+        return availabledoctors;
     }
 
     public Boolean evaluateOverLapTimeStamp(TimeSlot unavailableTImeSlot, TimeSlot requestedTimeSlot){
-        if(unavailableTImeSlot.getStartTime()==requestedTimeSlot.getStartTime())
+        if(Objects.equals(unavailableTImeSlot.getStartTime(), requestedTimeSlot.getStartTime()))
             return true;
         return false;
 
     }
+
+//    private void tryToBookFromWaitlist(String doctorName, TimeSlot timeSlot) {
+//        Queue<Appointment> waitQueue = waitList.get(doctorName);
+//        if (waitQueue != null && !waitQueue.isEmpty()) {
+//            Appointment nextAppointment = waitQueue.poll();
+//            if(Objects.equals(nextAppointment.getDoctor().getName(), doctorName)&& isTimeSlotEqual(timeSlot,nextAppointment.getTimeSlot())){
+//                markDoctorAvailability(doctorName, (List<TimeSlot>) nextAppointment.getTimeSlot());
+//                break;
+//
+//            }
+//
+//        }
+//
+//}
+
 
     public Boolean checkOverLapTimeSlot(TimeSlot givenTimeSlot, List<Appointment>appointments, Doctor doctor){
         Optional<Appointment> filteredAppointment =appointments.stream().filter(appointment -> evaluateOverLapTimeStamp(appointment.getTimeSlot(),givenTimeSlot)).filter(appointment -> !Objects.equals(appointment.getDoctor().getName(), doctor.getName())).findFirst();
@@ -75,33 +110,33 @@ public class AppointmentService {
 
     }
 
-//    public void addToWaitlist(String patientName, String doctorName, TimeSlot timeSlot){
-//        waitlist.get(doctorName).add({patientName,t})
-//    }
 
     public int bookAppointment(Integer bookingId, String patientName, String doctorName, TimeSlot timeSlot) throws Exception {
         Doctor doctor = findDoctorByName(doctorName);
         double slotTime = timeSlot.getEndTime()-timeSlot.getStartTime();
+        Patient patient = findOrCreatePatient(patientName);
+        Appointment appointment = new Appointment(bookingId,patient, doctor, timeSlot);
 
         if(slotTime==.50){
+
             if (isSlotAvailable(doctor, timeSlot)) {
-                Patient patient = findOrCreatePatient(patientName);
                 List<Appointment>patientAppointments = patient.getAppointments();
                 if(checkOverLapTimeSlot(timeSlot,patientAppointments,doctor)){
                     throw  new Exception("Appointment is Already Present for the Patient "+ patientName +" with Different Doctor within time slot");
                 }
-                Appointment appointment = new Appointment(bookingId,patient, doctor, timeSlot);
                 doctor.getAppointments().add(appointment);
                 removeSlotFromDoctorAvailability(doctor,timeSlot);
                 patient.getAppointments().add(appointment);
                 appointments.put(bookingId, appointment);
                 return bookingId;
             } else {
-//                addToWaitlist(patientName, doctorName, timeSlot);
+
+
+                addToWaitlist(appointment, doctorName);
                 return -1;
             }
         }else{
-            throw  new Exception("Slot must be of 30 minutes"+ slotTime );
+            throw  new Exception("Sorry, Dr "+doctorName+" Slot are 30 minutes only" );
         }
 
 
@@ -119,7 +154,8 @@ public class AppointmentService {
             doctor.getAppointments().remove(appointment);
             doctor.getAvailability().add(appointment.getTimeSlot());
             appointments.remove(bookingId);
-//            tryToBookFromWaitlist(doctor.getName());
+            System.out.println("Booking Cancelled");
+//            tryToBookFromWaitlist(doctor.getName(), appointment.getTimeSlot());
             return true;
         }
         return false;
@@ -184,7 +220,7 @@ public class AppointmentService {
         if(isDoctor){
             return doctors.stream().filter(doctor -> Objects.equals(doctor.getName(), name)).findFirst().get().getAppointments();
         }else{
-            return patients.stream().filter(patient -> Objects.equals(patient.getName(), name)).findFirst().get().getAppointments();
+            return getPatientAppointments(name);
         }
 
     }
